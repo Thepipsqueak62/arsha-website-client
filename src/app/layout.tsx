818b82c9f2989f41f9c9a8a5a383464a218794e5
Omni <98383696+Thepipsqueak62@users.noreply.github.com>
2026-03-30 21:30:42 -0400

    Commit project added test HeroSection.tsx and Darkmode theme-provider.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import {ThemeProvider} from "@/app/components/theme-provider";
+
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -28,9 +30,21 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+
+      >
+        {children}
+      </ThemeProvider>
+
+      </body>
     </html>
   );
 }
